778bad13d67c0bf4248b7582f7f73811c144c612
Seth Hall <seth@middle8media.com>
2012-06-22 14:40:36 -0400

    1st draft commit

--- naomi-1st-birthday/js/script-ck.js
+++
@@ -1,4 +0,0 @@
-/* Author:
-
-*///FitText
-$("h1").fitText(1,{minFontSize:"30px",maxFontSize:"60px"});$("h3").fitText(2.5,{minFontSize:"12px",maxFontSize:"23px"});$(document).ready(function(){$("#fitvid, #fitvid2").fitVids()});$(window).load(function(){$(".flexslider").flexslider()});
